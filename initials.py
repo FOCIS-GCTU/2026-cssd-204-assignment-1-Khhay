1cd4cf3be6c24292edc26646943d362dab84d820
Kelvin Amu <amukelvin80@gmail.com>
2026-05-30 17:27:48 -0700

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1 
+# 
+# Name: <KELVIN AMU>
+# STUDENT ID:  <2425403579>
+# Email: <amukelvin80@gmail.com/2425403579@gctu.edu.gh>
+# Grader: <AUGUSTUS> 
+# 
+# On my honor, <Kelvin AMU>, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This program prints the initials K, F, and A in a stylized block format.
+    print()
+    print("...KFA")
+    print()
+    print("...KKKK....KK..FFFFFFFFFFFF..AAAAAA....****")
+    print("...KKKK...KK..FFFFFFFFFFFF..AAAAAA....****")
+    print("...KK.KK.KK...FF............AA....AA..****")
+    print("...KK.KK.KK...FF............AA....AA..****")
+    print("...KK..KKK....FFFFFFFF......AAAAAAAA..****")
+    print("...KK..KKK....FFFFFFFF......AAAAAAAA..****")
+    print("...KK.KK.KK...FF............AA....AA..****")
+    print("...KKKK...KK..FF............AA....AA..****")
+    print("...KKKK....KK.FF............AA....AA..****")
+    print("...KKKK....KK.FF............AA....AA..****")
+    print()
 
 
 main()
+
+
